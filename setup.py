d5787b56ab32554d7154a3d4c7d0e374a4c7d564
Kazi-Ferdous-Hasan <ferdous.hasan.ewu@gmail.com>
2024-02-29 14:24:19 +0600

    requirements update

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'RS ChatBot',
+    version= '0.0.0',
+    author= 'Kazi Ferdous Hasan',
+    author_email= 'kfh.ewu@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+)
